37be4b9ca8d3042eeaeb697fe7a0e4a493310821
Tom L. <devzemux1613@gmail.com>
2024-07-25 16:15:15 +0200

    refactor: using build in functions

--- src/main/java/de/proxysystem/utils/TimeProvider.java
+++ src/main/java/de/proxysystem/utils/TimeProvider.java
@@ -1,33 +1,30 @@
 package de.proxysystem.utils;
 
+import java.util.concurrent.TimeUnit;
+
 public class TimeProvider {
 
   public static Long getSeconds(Long millis) {
-    return ((millis / 1000) % 60);
+    return TimeUnit.MILLISECONDS.toSeconds(millis);
   }
 
   public static Long getMinutes(Long millis) {
-    return ((millis / 1000) / 60) % 60;
+    return TimeUnit.MILLISECONDS.toMinutes(millis);
   }
 
   public static Long getHours(Long millis) {
-    return (((millis / 1000) / 60) / 60) % 24;
+    return TimeUnit.MILLISECONDS.toHours(millis);
   }
 
   public static Long getDays(Long millis) {
-    return (((millis / 1000) / 60) / 60) / 24;
+    return TimeUnit.MILLISECONDS.toDays(millis);
   }
 
   public static String getFormatedTime(Long millis) {
-    long milli = millis;
-    milli /= 1000;
-    long seconds = (milli % 60);
-    milli /= 60;
-    long minutes = (milli % 60);
-    milli /= 60;
-    long hours = (milli % 24);
-    milli /= 24;
-    long days = milli;
+    long days = getDays(millis);
+    long hours = getHours(millis);
+    long minutes = getMinutes(millis);
+    long seconds = getSeconds(millis);
 
     if (days != 0) {
       return days + " Tag(e), " + hours + " Stunde(n) und " + minutes + " Minute(n)";
@@ -36,26 +33,13 @@ public class TimeProvider {
     if (hours != 0) {
       return hours + " Stunde(n) und " + minutes + " Minute(n)";
     }
+
     if (minutes != 0) {
       return minutes + " Minute(n) und " + seconds + " Sekunde(n)";
     }
-    return days + " Tag(e), " + hours + " Stunde(n) und " + minutes + " Minute(n)";
-  }
 
-  public static String getTimeStringWithHighestTimeForm(Long millis) {
-    long days = getDays(millis);
-    long hours = getHours(millis);
-    long minutes = getMinutes(millis);
-
-    if (days != 0) {
-      return days + " Tag" + (days > 1 ? "e" : "");
-    }
-
-    if (hours != 0) {
-      return hours + " Stunde" + (hours > 1 ? "n" : "");
-    }
-    if (minutes != 0) {
-      return minutes + " Minute" + (minutes > 1 ? "n" : "");
+    if(seconds != 0) {
+      return seconds + " Sekunde(n)";
     }
 
     return days + " Tag(e), " + hours + " Stunde(n) und " + minutes + " Minute(n)";
